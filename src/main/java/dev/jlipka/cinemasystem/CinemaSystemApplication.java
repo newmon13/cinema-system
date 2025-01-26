5cfc6df5ae044e57aa19cecb69a49cd37f7f6b47
newmo13 <kubalipka123@gmail.com>
2025-01-26 17:55:19 +0100

    Initial commit

---
+++ src/main/java/dev/jlipka/cinemasystem/CinemaSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.jlipka.cinemasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSystemApplication.class, args);
+    }
+
+}
